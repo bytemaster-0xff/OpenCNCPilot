d9a0fe9f2bf0b832a4caaf390b368de7d66098da
Kevin D Wolf <kevinw@software-logistics.com>
2017-02-24 08:30:07 -0500

    rendering board outline w/ major hack

--- OpenCNCPilot/Controls/HeightMapControl.xaml.cs
+++ OpenCNCPilot/Controls/HeightMapControl.xaml.cs
@@ -7,6 +7,9 @@ using System.Xml.Linq;
 using LagoVista.EaglePCB.Managers;
 using HelixToolkit.Wpf;
 using System.Windows.Media;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 
 namespace LagoVista.GCode.Sender.Application.Controls
 {
@@ -28,11 +31,12 @@ namespace LagoVista.GCode.Sender.Application.Controls
         {
             var linePoints = new Point3DCollection();
 
-            var doc = XDocument.Load("./KegeratorController.brd");
+              var doc = XDocument.Load("./KegeratorController.brd");
+           // var doc = XDocument.Load("./EagleSample.brd");
             var pcb = EagleParser.ReadPCB(doc);
 
             var modelGroup = new Model3DGroup();
-            var silverMaterial = MaterialHelper.CreateMaterial(Colors.Silver);
+            var silverMaterial = MaterialHelper.CreateMaterial(Color.FromRgb(0xb8, 0x73, 0x33));
             var redMaterial = MaterialHelper.CreateMaterial(Colors.Red);
             var greenMaterial = MaterialHelper.CreateMaterial(Colors.Green);
             var blueMaterial = MaterialHelper.CreateMaterial(Colors.Blue);
@@ -45,40 +49,63 @@ namespace LagoVista.GCode.Sender.Application.Controls
                     foreach (var pad in element.SMDPads)
                     {
                         var padMeshBuilder = new MeshBuilder(false, false);
-                        padMeshBuilder.AddBox(new Point3D(pad.X, pad.Y, 1), pad.DX, pad.DY, 1);
+                        padMeshBuilder.AddBox(new Point3D(pad.X, pad.Y, 1), pad.DX, pad.DY, 0.25);
                         modelGroup.Children.Add(new GeometryModel3D() { Geometry = padMeshBuilder.ToMesh(true), Material = silverMaterial });
                     }
                 }
-
-                /*
-                foreach (var circle in element.Package.Circles)
-                {
-                    var circleMeshBuilder = new MeshBuilder(false, false);
-                    circleMeshBuilder.AddCylinder(new Point3D(circle.X, circle.Y, 0), new Point3D(circle.X, circle.Y, 3), circle.Radius);
-                    modelGroup.Children.Add(new GeometryModel3D() { Geometry = circleMeshBuilder.ToMesh(true), Material = blackMaterial });
-                }
-
-                foreach (var circle in element.Package.Holes)
-                {
-                    var circleMeshBuilder = new MeshBuilder(false, false);
-                    circleMeshBuilder.AddCylinder(new Point3D(circle.X, circle.Y, 0), new Point3D(circle.X, circle.Y, 3), circle.Drill);
-                    modelGroup.Children.Add(new GeometryModel3D() { Geometry = circleMeshBuilder.ToMesh(true), Material = blackMaterial });
-                }*/
-
-                var boardEdgeMeshBuilder = new MeshBuilder(false, false);
-                var outlineWires = pcb.Layers.Where(layer => layer.Number == 20).FirstOrDefault().Wires;
-
-                boardEdgeMeshBuilder.AddBox(new Point3D(50, 40, 0), 100, 68, 1);
-                boardEdgeMeshBuilder.AddBox(new Point3D(50, 3, 0), 88, 6, 1);
-                boardEdgeMeshBuilder.AddBox(new Point3D(50, 77, 0), 88, 6, 1);
-
-                boardEdgeMeshBuilder.AddCylinder(new Point3D(6, 6, -0.5),new Point3D(6,6,0.5),6, 50, true, true);
-                boardEdgeMeshBuilder.AddCylinder(new Point3D(6, 74, -0.5), new Point3D(6, 74, 0.5), 6, 50, true, true);
-                boardEdgeMeshBuilder.AddCylinder(new Point3D(94, 6, -0.5), new Point3D(94, 6, 0.5), 6, 50, true, true);
-                boardEdgeMeshBuilder.AddCylinder(new Point3D(94, 74, -0.5), new Point3D(94, 74, 0.5), 6, 50, true, true);
-                modelGroup.Children.Add(new GeometryModel3D() { Geometry = boardEdgeMeshBuilder.ToMesh(true), Material = greenMaterial });
-
             }
+            /*
+            foreach (var circle in element.Package.Circles)
+            {
+                var circleMeshBuilder = new MeshBuilder(false, false);
+                circleMeshBuilder.AddCylinder(new Point3D(circle.X, circle.Y, 0), new Point3D(circle.X, circle.Y, 3), circle.Radius);
+                modelGroup.Children.Add(new GeometryModel3D() { Geometry = circleMeshBuilder.ToMesh(true), Material = blackMaterial });
+            }
+            */
+            foreach (var circle in pcb.Holes)
+            {
+                var circleMeshBuilder = new MeshBuilder(false, false);
+                circleMeshBuilder.AddCylinder(new Point3D(circle.X, circle.Y, -0.5), new Point3D(circle.X, circle.Y, 0.51), circle.Drill / 2);
+                modelGroup.Children.Add(new GeometryModel3D() { Geometry = circleMeshBuilder.ToMesh(true), Material = blackMaterial });
+
+                Debug.WriteLine(circle.X + " " + circle.Y); ;
+            }
+
+            var boardEdgeMeshBuilder = new MeshBuilder(false, false);
+
+            #region Hold your nose to discover why irregular boards don't render as expected... 
+            /* gonna cheat here in next chunk of code...need to make progress, assume all corners are
+             * either square or round.  If rounded, same radius...WILL revisit this at some point, KDW 2/24/2017
+             * FWIW - feel so dirty doing this, but need to move on :*( 
+             * very happy to accept a PR to fix this!  Proper mechanism is to create a polygon and likely subdivide the curve into smaller polygon edges
+             * more work than it's worth right now....sorry again :(
+             */
+            //TODO: Render proper edge of board.
+
+
+            var cornerWires = pcb.Layers.Where(layer => layer.Number == 20).FirstOrDefault().Wires.Where(wire => wire.Curve.HasValue == true);
+            var radius = cornerWires.Any() ?  cornerWires.First().Rect.X2 : 0;
+            if(radius == 0)
+            {
+                boardEdgeMeshBuilder.AddBox(new Point3D(pcb.Width / 2, pcb.Height / 2, 0), pcb.Width, pcb.Height, 1);
+            }
+            else
+            {
+                boardEdgeMeshBuilder.AddBox(new Point3D(pcb.Width / 2, pcb.Height / 2, 0), pcb.Width - (radius * 2), pcb.Height - (radius * 2), 1);
+                boardEdgeMeshBuilder.AddBox(new Point3D(pcb.Width / 2, radius / 2, 0), pcb.Width - (radius * 2), radius, 1);
+                boardEdgeMeshBuilder.AddBox(new Point3D(pcb.Width / 2, pcb.Height -  radius / 2, 0), pcb.Width - (radius * 2), radius, 1);
+                boardEdgeMeshBuilder.AddBox(new Point3D(radius / 2, pcb.Height / 2, 0), radius, pcb.Height - (radius * 2),  1);
+                boardEdgeMeshBuilder.AddBox(new Point3D(pcb.Width - radius / 2, pcb.Height / 2, 0), radius, pcb.Height - (radius * 2), 1);
+                boardEdgeMeshBuilder.AddCylinder(new Point3D(radius, radius, -0.5), new Point3D(radius, radius, 0.5), radius, 50, true, true);
+                boardEdgeMeshBuilder.AddCylinder(new Point3D(radius, pcb.Height - radius, -0.5), new Point3D(radius, pcb.Height - radius, 0.5), radius, 50, true, true);
+                boardEdgeMeshBuilder.AddCylinder(new Point3D(pcb.Width - radius, radius, -0.5), new Point3D(pcb.Width - radius, radius, 0.5), radius, 50, true, true);
+                boardEdgeMeshBuilder.AddCylinder(new Point3D(pcb.Width - radius, pcb.Height - radius, -0.5), new Point3D(pcb.Width - radius, pcb.Height - radius, 0.50), radius, 50, true, true);
+            }
+            #endregion
+
+            modelGroup.Children.Add(new GeometryModel3D() { Geometry = boardEdgeMeshBuilder.ToMesh(true), Material = greenMaterial });
+
+    
 
             PadsLayer.Content = modelGroup;
         }
