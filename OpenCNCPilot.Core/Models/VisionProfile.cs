e6a927c85bf966c644cc0896f7be75524138526c
Kevin D Wolf <kevinw@software-logistics.com>
2017-03-14 14:03:42 -0400

    add backing stores to vision profile

--- OpenCNCPilot.Core/Models/VisionProfile.cs
+++ OpenCNCPilot.Core/Models/VisionProfile.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LagoVista.Core.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,17 +7,17 @@ using System.Threading.Tasks;
 
 namespace LagoVista.GCode.Sender.Models
 {
-    public class VisionProfile
+    public class VisionProfile : ModelBase
     {
-        public void RaisePropertyChanged()
+        private string _name;
+        public string Name
         {
-
+            get { return _name; }
+            set { _name = value;  RaisePropertyChanged(); }
         }
 
-        public string Name { get; set; }
-
         private double _polygonEpsilon = 0;
-        public double PolygonEpsilon
+        public double PolygonEpsilon                                                 
         {
             get { return _polygonEpsilon; }
             set { _polygonEpsilon = value; RaisePropertyChanged(); }
@@ -28,39 +29,97 @@ namespace LagoVista.GCode.Sender.Models
             get { return _harrisCornerApeture; }
             set { _harrisCornerApeture = value; RaisePropertyChanged(); }
         }
-        private int _harrisCornerBlockSize = 0;
-        public double HarrisCornerBlockSize { get; set; }
+        private double _harrisCornerBlockSize = 0;
+        public double HarrisCornerBlockSize
+        {
+            get { return _harrisCornerBlockSize; }
+            set { _harrisCornerBlockSize = value; RaisePropertyChanged(); }
+        }
+
         private int _harrisCornerK = 0;
-        public int HarrisCornerK { get; set; }
+        public int HarrisCornerK
+        {
+            get { return _harrisCornerK; }
+            set { _harrisCornerK = value; RaisePropertyChanged(); }
+        }
 
         private int _guassianKSize = 5;
-        public int GaussianKSize { get; set; } = 5;
+        public int GaussianKSize
+        {
+            get { return _guassianKSize; }
+            set { _guassianKSize = value; RaisePropertyChanged(); }
+        }
         private double _guassianSigmaX = 2;
-        public double GaussianSigmaX { get; set; } = 2;
+        public double GaussianSigmaX
+        {
+            get { return _guassianSigmaX; }
+            set { _guassianSigmaX = value; RaisePropertyChanged(); }
+        }
         private double _guasianSigmaY = 2;
-        public double GaussianSigmaY { get; set; } = 2;
+        public double GaussianSigmaY
+        {
+            get { return _guasianSigmaY; }
+            set { _guasianSigmaY = value; RaisePropertyChanged(); }
+        }
 
         private double _cannyLowThreshold = 5;
-        public double CannyLowThreshold { get; set; } = 5;
-        private double _cannyHighThreshold = 3 * 5;
+        public double CannyLowThreshold
+        {
+            get { return _cannyLowThreshold; }
+            set
+            {
+                _cannyLowThreshold = value;
+                RaisePropertyChanged();
+                RaisePropertyChanged(nameof(CannyHighThreshold));
+            }
+        }
+
         public double CannyHighThreshold { get { return CannyLowThreshold * 3; } set { } }
         private int _cannyApetureSize = 3;
-        public int CannyApetureSize { get; set; } = 3;
-        private bool CannyGrandian = true;
-        public bool CannyGradient { get; set; }
+        public int CannyApetureSize
+        {
+            get { return _cannyApetureSize; }
+            set { _cannyApetureSize = value; RaisePropertyChanged(); }
+        }
+
+        private bool _cannyGradiant = true;
+        public bool CannyGradient
+        {
+            get { return _cannyGradiant; }
+            set { _cannyGradiant = value; RaisePropertyChanged(), }
+        }
 
 
         private double _houghLinesRHO = 1;
-        public double HoughLinesRHO { get; set; } = 1;
+        public double HoughLinesRHO
+        {
+            get { return _houghLinesRHO; }
+            set { _houghLinesRHO = value; RaisePropertyChanged(); }
+        }
         private double _houghLinesTheta = 0;
-        public double HoughLinesTheta { get; set; }
+        public double HoughLinesTheta
+        {
+            get { return _houghLinesTheta; }
+            set { _houghLinesTheta = value; RaisePropertyChanged(); }
+        }
         private int _houghLinesThreshold = 0;
-        public int HoughLinesThreshold { get; set; }
+        public int HoughLinesThreshold
+        {
+            get { return _houghLinesThreshold; }
+            set { _houghLinesThreshold = value; RaisePropertyChanged(); }
+        }
         private double _houghLinesMinLength = 5;
-        public double HoughLinesMinLineLength { get; set; }
+        public double HoughLinesMinLineLength
+        {
+            get { return _houghLinesMinLength; }
+            set { _houghLinesMinLength = value; RaisePropertyChanged(); }
+        }
         private double _houghLinesMaxLineGap = 5;
-        public double HoughLinesMaxLineGap { get; set; }
-
+        public double HoughLinesMaxLineGap
+        {
+            get { return _houghLinesMaxLineGap; }
+            set { _houghLinesMaxLineGap = value; RaisePropertyChanged(); }
+        }
 
         private double _houghCirclesDP = 2;
         public double HoughCirclesDP
