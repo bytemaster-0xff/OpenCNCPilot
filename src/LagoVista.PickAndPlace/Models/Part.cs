d2eed8185972566f2f4a82803899b604e732beb2
Kevin D Wolf <kevinw@software-logistics.com>
2017-04-28 08:06:19 -0400

    add PNP Project

---
+++ src/LagoVista.PickAndPlace/Models/Part.cs
@@ -0,0 +1,18 @@
+﻿using LagoVista.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LagoVista.PickAndPlace.Models
+{
+    public class Part : ModelBase
+    {
+        public Package Package { get; set; }
+
+        public string Value { get; set; }
+
+        public string PartNumber { get; set; }
+    }
+}
---
+++ src/LagoVista.PickAndPlace/Models/PartsTrayDefinition.cs
@@ -0,0 +1,19 @@
+﻿using LagoVista.Core.Models.Drawing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LagoVista.PickAndPlace.Models
+{
+    /// <summary>
+    /// Defines the Physical Definition of a Parts Tray
+    /// </summary>
+    public class PartsTrayDefinition
+    {
+        public int Rows { get; set; }
+
+        public Vector2 Size { get; set; }
+    }
+}
